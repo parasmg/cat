14964ae6a39ce6de6516371c1e767052f841562a
Paras Gandhi <pparas@gmail.com>
2018-04-13 13:05:27 -0400

    -Added and completed rich edit functionality
    +need to optimize the search functionality
    
     Changes to be committed:
    	modified:   content/js/controllers/details.js
    	modified:   content/js/controllers/richEditController.js
    	modified:   content/js/lib/richEditDirective.js
    	modified:   content/js/plugins/tinymce.min.js

--- content/js/controllers/richEditController.js
+++ content/js/controllers/richEditController.js
@@ -1,25 +1,50 @@
-catalogEditor.controller('richEditController', ['$scope', 'TinymceFactory',
-function ($scope, TinymceFactory) {
+catalogEditor.controller('richEditController', ['$scope', '$sce', '$timeout',
+function ($scope, $sce, $timeout) {
+
     this.myObject = $scope.myObject;
     this.myKey = $scope.myKey;
     this.myValue = $scope.myValue;
     this.editMode = false;
     this.old = $scope.myValue;
 
-    this.openeditor = function(){
+    this.updateHtml = function (src) {
+        this.tinymceHtml = $sce.trustAsResourceUrl(src);
+    }
+
+    $scope.tinymceOptions = {
+        setup: function (editor) {
+            $timeout(function () {
+                editor.focus();
+            }, 200)
+        },
+        plugins: ['advlist autolink lists link image charmap print preview anchor',
+                  'searchreplace visualblocks code fullscreen',
+                  'insertdatetime media table contextmenu paste code',
+        ],
+        toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat code |',
+        statusbar: false,
+        menubar: false,
+        height: 250,
+        anchor_bottom: false,
+        anchor_top: false
+    };
+    
+    this.getContent = function () {
+        
+    };
+
+    this.openeditor = function () {
         this.editMode = true;
-        console.log("EDIT MODE:",this.editMode);
+        console.log("EDIT MODE:", this.editMode);
     };
 
     this.save = function () {
         this.editMode = false;
+        console.log('Editor content:', this.tinymceModel);
         this.old = this.myValue;
-        console.log("Model:", this.myValue);
     }
 
     this.cancel = function () {
-        console.log("Old Value:", this.old);
-        console.log("New Value:", this.myValue);
         this.myValue = this.old;
         this.editMode = false;
     }
--- content/js/lib/richEditDirective.js
+++ content/js/lib/richEditDirective.js
@@ -1,34 +1,17 @@
-catalogEditor.directive('richEdit', ['$timeout', 'TinymceFactory',
-    function ($timeout, TinymceFactory) {
+catalogEditor.directive('richEdit', ['$timeout', 
+    function ($timeout) {
 
         return {
             restrict: 'E',
             scope: {
                 myObject: '=',
                 myValue: '=',
-                myKey:'='
+                myKey: '='
             },
             templateUrl: '/content/views/richEditTemplate.html',
             controller: 'richEditController as rich',
             link: function ($scope, $element, $attrs) {
-
-                var editElement = $element[0].querySelector('textarea');
-                console.log('Found Text Area: ', editElement.id);
-
-                TinymceFactory({
-                    selector: '#'+editElement.id,
-                    plugins: [
-                      'advlist autolink lists link image charmap print preview anchor',
-                      'searchreplace visualblocks code fullscreen',
-                      'insertdatetime media table contextmenu paste code'
-                    ],
-                    font_formats: 'Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;AkrutiKndPadmini=Akpdmi-n' //, height: 500
-                }).then(function (editors) {
-                    console.log("Editors:",editors);
-                })
-                
-                
-                
+               
            }
         }
     }
